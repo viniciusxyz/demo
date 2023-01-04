8fbed27a0802e21a66f27a3e6db4ec866d9f94f2
Vinicius Vieira dos Santos <viniciusxyz>
2023-01-04 02:17:28 +0000

    Initial commit

---
+++ src/main/java/vvsantos/fake/oauth2/server/Application.java
@@ -0,0 +1,10 @@
+package vvsantos.fake.oauth2.server;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
